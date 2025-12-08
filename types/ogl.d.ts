f65a4a50fc2ff92edd0f975f5fefe3a5723b589b
PaulWallaceATL <Paul@tryclinixai.com>
2025-12-08 17:05:10 -0500

    chore(types): add ogl ambient declaration for particles

---
+++ types/ogl.d.ts
@@ -0,0 +1,25 @@
+declare module "ogl" {
+  export class Renderer {
+    constructor(options?: any);
+    gl: WebGLRenderingContext;
+    setSize(width: number, height: number): void;
+  }
+  export class Camera {
+    constructor(gl: WebGLRenderingContext, options?: any);
+    position: { set: (x: number, y: number, z: number) => void };
+    perspective(options: { aspect: number }): void;
+  }
+  export class Geometry {
+    constructor(gl: WebGLRenderingContext, attributes: Record<string, any>);
+  }
+  export class Program {
+    constructor(gl: WebGLRenderingContext, options: any);
+    uniforms: Record<string, any>;
+  }
+  export class Mesh {
+    constructor(gl: WebGLRenderingContext, options: any);
+    position: { x: number; y: number };
+    rotation: { x: number; y: number; z: number };
+  }
+}
+
